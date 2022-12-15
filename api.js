d00be206b365b935aef501e40a701f056e0fbc02
helen121027 <helen121027@gmail.com>
2022-12-15 23:39:03 +0900

    초기 세팅 완료

---
+++ api.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'http://localhost:3000';
+
+const fetcher = async url => {
+  try {
+    const response = await fetch(`${BASE_URL}${url}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throw new Error('API 통신 실패');
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const getProducts = () => fetcher('/products');
+export const getProduct = id => fetcher(`/products/${id}`);
